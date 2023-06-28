18652777bc8c5fbbab9a0a2aa1bb7f905082d252
sathish19961412 <sathishfreelanc5@gmail.com>
2023-06-28 12:59:08 +0530

    codeignter config with database with routes to the controller with view in header and footer

---
+++ application/views/header.php
@@ -0,0 +1,14 @@
+<!Doctype html>
+<html lang="en">
+	<head>
+       <meta charset="utf-8">
+	   <meta name="viewport" content="width=device-width;initial-scale=1.0">
+	   <title>Page Title</title>
+    </head>
+	<style>
+	</style>
+	<body>
+		<script>
+		</script>
+    </body>
+</html>
